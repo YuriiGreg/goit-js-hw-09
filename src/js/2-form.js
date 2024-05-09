e04c20929864e41afdcba1f30a0352826be53c53
YuriiGreg <yuriyf888@gmail.com>
2024-05-10 01:24:44 +0200

    hw-09

---
+++ src/js/2-form.js
@@ -0,0 +1,32 @@
+
+let formData = {
+  email: '',
+  message: ''
+};
+
+if (localStorage.getItem('feedback-form-state')) {
+  formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  document.querySelector('input[name="email"]').value = formData.email;
+  document.querySelector('textarea[name="message"]').value = formData.message;
+}
+
+document.querySelector('.feedback-form').addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+document.querySelector('.feedback-form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  formData = { email: '', message: '' };
+  document.querySelector('.feedback-form').reset();
+});
